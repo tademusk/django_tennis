ecefb84ef630d0fcf1123c3b04089f09d62f2e25
tademusk <tadelsaloove@gmail.com>
2025-01-29 14:51:54 +0300

    supporting templates adeded

--- members/urls.py
+++ members/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('members/', views.members, name = "members"),
     path('', views.first, name = "first"),
+    path('members/details/<int:id>', views.details, name='details'),
+    path('testing/', views.testing, name='testing'),  
 ]
 
--- members/views.py
+++ members/views.py
@@ -1,10 +1,34 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from .models import Member
 
 def members(request):
-    return HttpResponse("Hello there")
+    mymembers = Member.objects.all().values()
+    template = loader.get_template('all_members.html')
+    context = {
+        "hello" : mymembers,
+    }
+    return HttpResponse(template.render(context, request))
 
 def first(request):
     template = loader.get_template('first.html')
     return HttpResponse(template.render())
+
+
+def details(request, id):
+  mymember = Member.objects.get(id=id)
+  template = loader.get_template('details.html')
+  context = {
+    'mymember': mymember,
+  }
+  return HttpResponse(template.render(context, request))
+
+def testing(request):
+  template = loader.get_template('template.html')
+  context = {
+    'x': ['Apple', 'Banana', 'Cherry'], 
+    'y': ['Apple', 'Banana', 'Cherry'], 
+  }   
+
+  return HttpResponse(template.render(context, request))

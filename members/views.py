c45d1c68ea219bb6ee263ec7fb05ca9fd74c1cd1
tademusk <tadelsaloove@gmail.com>
2025-01-29 15:31:47 +0300

    footer and header

--- members/views.py
+++ members/views.py
@@ -4,7 +4,7 @@ from django.template import loader
 from .models import Member
 
 def members(request):
-    mymembers = Member.objects.all().values()
+    mymembers = Member.objects.filter(firstname='Tade').values()
     template = loader.get_template('all_members.html')
     context = {
         "hello" : mymembers,

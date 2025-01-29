bcd56918b4154c6caf991bdcbe7f7a461e399815
tademusk <tadelsaloove@gmail.com>
2025-01-29 19:01:06 +0300

    static file collection

--- members/views.py
+++ members/views.py
@@ -4,7 +4,7 @@ from django.template import loader
 from .models import Member
 
 def members(request):
-    mymembers = Member.objects.filter(firstname='Tade').values()
+    mymembers = Member.objects.all().values()
     template = loader.get_template('all_members.html')
     context = {
         "hello" : mymembers,
@@ -14,7 +14,6 @@ def members(request):
 def first(request):
     template = loader.get_template('first.html')
     return HttpResponse(template.render())
-
 
 def details(request, id):
   mymember = Member.objects.get(id=id)
--- my_tennis_club/settings.py
+++ my_tennis_club/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!s2e902hex3$h25p^75!(%!^e3#$@6uvlj=f2_@@g)cz^$q*u('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'my_tennis_club.urls'
@@ -116,7 +117,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
+STATIC_ROOT = BASE_DIR / 'productionfiles'
+
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'mystaticfiles'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
